b4da109b863ae5de4093be0ddb474badec5037b0
KEROLIS <kerlisyoussif12@gmail.com>
2023-02-12 14:56:01 +0200

    first commit Changes to be committed: 	new file:   facial_recognition.py 	new file:   re_api.py 	new file:   requirements.txt

---
+++ facial_recognition.py
@@ -0,0 +1,129 @@
+from deepface import DeepFace
+from deepface.commons import distance
+from fastapi import HTTPException
+
+import cv2
+import os
+
+models = [
+    "VGG-Face",
+    "Facenet",
+    "Facenet512",
+    "OpenFace",
+    "DeepFace",
+    "DeepID",
+    "ArcFace",
+    "Dlib",
+    "SFace",
+]
+
+
+def compare_features(features, db_features, distance_metric='cosine'):
+
+    if distance_metric == 'cosine':
+        dist = distance.findCosineDistance(features, db_features)
+    elif distance_metric == 'euclidean':
+        dist = distance.findEuclideanDistance(features, db_features)
+    elif distance_metric == 'euclidean_l2':
+        dist = distance.findEuclideanDistance(distance.l2_normalize(
+            features), distance.l2_normalize(db_features))
+    else:
+        raise ValueError("Invalid distance_metric passed - ", distance_metric)
+
+    return dist
+
+
+def add_p(person_name, face_image):
+    '''
+    Add a person's face to the database
+    person_name: str, name of the person
+    face_image: str, path to the image of the face
+    '''
+    # extract features from the face image
+    embedding_objs = DeepFace.represent(face_image, model=models[0])
+    features = embedding_objs[0]["embedding"]
+
+    # write the features to the database file
+    try:
+        with open('face_db.txt', 'a') as f:
+            f.write(f'{person_name}: {features}\n')
+    except FileNotFoundError:
+        raise HTTPException(
+            status_code=400, detail="Error while adding the person features ")
+
+
+def delete_p(person_name):
+    '''
+    Delete a person from the database
+    person_name: str, name of the person
+    '''
+    # read the database into a list of lines
+    try:
+        with open("face_db.txt", "r") as f:
+            lines = f.readlines()
+    except FileNotFoundError:
+        raise HTTPException(status_code=400, detail="Database not found")
+
+    person_found = False
+    # write back the lines without the line for the person to delete
+    with open('face_db.txt', 'w') as f:
+        for line in lines:
+            if not line.startswith(person_name):
+                f.write(line)
+            else:
+                person_found = True
+    return person_found
+
+
+def recognize_p(face_image, distance_metric, threshold):
+    '''
+    Recognize a person in the image
+    face_image: str, path to the image of the face
+    distance_metric:  str, the way to calculate the distnce between 2 embeddings 
+    threshold:  float, the maximum value to determine if the person is verfied or not based on calculated distance between embeddings
+    '''
+    # extract features from the face image
+    embedding_objs = DeepFace.represent(face_image, model=models[0])
+    features = embedding_objs[0]["embedding"]
+
+    # read the database into a list of lines
+    try:
+        with open("face_db.txt", "r") as f:
+            lines = f.readlines()
+    except FileNotFoundError:
+        raise HTTPException(status_code=400, detail="Database not found")
+
+    # compare the features of the face image with the features in the database
+    min_distance = float('inf')
+    recognized_person = None
+    for line in lines:
+        line = line.strip().split(': ')
+        person_name = line[0]
+        db_features = eval(line[1])
+        dist = compare_features(features, db_features)
+
+        # get the value of threshold based on the model and distance_metric if it's not given by the user
+        if threshold is None:
+            threshold = distance.findThreshold(models[0], distance_metric)
+
+        if dist < min_distance and dist < threshold:
+            min_distance = dist
+            recognized_person = person_name
+
+    # return the name of the recognized person
+    return recognized_person
+
+
+def list_p():
+    '''
+    List all the persons in the database
+    '''
+    # read the database into a list of lines
+    try:
+        with open("face_db.txt", "r") as f:
+            lines = f.readlines()
+    except FileNotFoundError:
+        raise HTTPException(status_code=400, detail="Database file not found")
+
+    # return the names of all the persons
+    return [line.strip().split(': ')[0] for line in lines]
---
+++ re_api.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI, UploadFile, HTTPException
+import numpy as np
+import cv2
+from facial_recognition import add_p, delete_p, recognize_p, list_p
+
+app = FastAPI()
+
+
+@app.post("/add_person")
+async def add_person(person_name: str, file: UploadFile):
+    ''' function takes a person_name string and an UploadFile as parameters.
+         It uses cv2 to decode the file into an image and passes it along with the person_name to the add_p() 
+         function from the facial_recognition module. It then returns a message confirming that the person was added to the database.
+    '''
+    face_image = cv2.imdecode(np.frombuffer(await file.read(), np.uint8), -1)
+    add_p(person_name, face_image)
+    return {"message": f"Person {person_name} added to the database"}
+
+
+@app.delete("/delete_person/{person_name}")
+def delete_person(person_name: str):
+    ''' This function takes a person_name string as a parameter and passes it to 
+        the delete_p() function from the facial_recognition module. 
+        If the person is found in the database, it returns a message confirming that they were deleted;
+         otherwise, it raises an HTTPException with status code 400 and an error message. 
+    '''
+
+    # check and delete the given person name from datbase
+    person_found = delete_p(person_name)
+
+    if person_found:
+        return {"message": f"Person {person_name} deleted from the database"}
+    else:
+        raise HTTPException(
+            status_code=400, detail=f"Person {person_name} not found in the database")
+
+
+@app.post("/recognize_person/")
+async def recognize_person(file: UploadFile, distance_metric: str = "cosin", threshold: float = None):
+    ''' This function takes an UploadFile and two optional parameters: distance metric (defaults to "cosin") and threshold (defaults to None). 
+            It uses cv2 to decode the file into an image and passes it along with distance metric and threshold to recognize_p() from facial_recognition module.
+            It then returns either the recognized person or "No matching person found". 
+    '''
+    face_image = cv2.imdecode(np.frombuffer(await file.read(), np.uint8), -1)
+    recognized_person = recognize_p(face_image, distance_metric, threshold)
+    return {"recognized_person": recognized_person if recognized_person else "No matching person found"}
+
+
+@app.get("/list_persons/")
+async def list_persons():
+    '''This finction retreves all the stored users names in the database'''
+    return {"persons": list_p()}
